7e2a522d8bcaf87e11909ce0acf280bc409e40de
om waman <50038648+omwaman1@users.noreply.github.com>
2020-05-17 15:07:04 +0530

    Add files via upload

---
+++ mainn.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<style>
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+input[type=number], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+input[type=submit] {
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+div {
+	width=: 140px;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 100px;
+}
+</style>
+<body>
+<div>
+  <form action="insert.php" method="post"></br>
+    <label for="runner_name">Runner Name:-</label>
+    <input type="text" id="runner_name" name="runner_name" placeholder="Runner name.."> </br></br>
+</br>
+    <label for="cand_name">Candidate Name:-</label>
+    <input type="text" id="cand_name" name="cand_name" placeholder="Candidate Name..."> </br></br>
+ </br> 	<label for="Case ID">Case ID:'</label>
+    <input type="text" id="caseid" name="caseid" placeholder="enter Case ID...."> </br></br>
+       <label for="father_name">Father Name:-</label>
+    <input type="text" id="father_name" name="father_name" placeholder="enter father name.."> </br></br>
+</br>
+   <label for="mother_name">Mother Name:-</label>
+    <input type="text" id="mother_name" name="mother_name" placeholder="Mother name.."> </br></br>
+</br>
+   <label for="own">Ownership:-</label>
+    <input type="text" id="own" name="own" placeholder="Own or Rent.."> </br></br>
+</br>
+   <label for="fname">Period of stay</label>
+    <input type="text" id="period" name="period" placeholder="enter period of stay.."> </br></br>
+</br>
+   <label for="verifire">Verifire Name:-</label>
+    <input type="text" id="verifire" name="verifire" placeholder="enter Verifire name.."> </br></br>
+</br>
+   <label for="fname">Verifire's contact:-</label>
+    <input type="number" id="verifirec" name="verifirec" placeholder="enter Verifier's contact.."> </br></br>
+</br>
+   <label for="rwcandidate">Relationship With Candidate:-</label>
+    <input type="text" id="rwcandidate" name="rwcandidate" placeholder="Relationship With Candidate.."> </br></br>
+</br>
+   <label for="nland">Nearby Landmark</label>
+    <input type="text" id="nland" name="nlandmark" placeholder="enter nearby landmark.."> </br></br>
+</br>
+  </br>  <input type="submit" value="Submit">
+  </form>
+</div>
+
+</body>
+</html>
